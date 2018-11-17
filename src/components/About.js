faa0a02014c257eb075548542da1098e170724b7
Deepak Tyagi <depaktyagi1096@gmail.com>
2018-11-17 17:14:33 +0530

    React router functionality tested

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+	return(
+		<div>
+			<h1>About Page</h1>
+			<p>This is About Page with path '/'</p>
+		</div>
+	);
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contact = () => {
+	return(
+		<div>
+			<h1>Contact Page</h1>
+			<p>This is Contact Page with path '/'</p>
+		</div>
+	);
+};
+
+export default Contact;
